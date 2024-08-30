5c986afe97f0a54cd9c83cec1470ebd73318d27d
yjunechoe <jchoe001@gmail.com>
2024-08-30 17:27:04 -0400

    export read_pcibex

--- R/read.R
+++ R/read.R
@@ -10,15 +10,17 @@
 #'   since been fixed, so the value is set to `FALSE` by default.
 #' @param type_opts A list of arguments passed to `type.convert()`, or `readr::type_convert()` if tidyverse is installed.
 #'
+#' @export
+#'
 #' @return A dataframe
 read_pcibex <- function(file, encoding = "UTF-8", exclude_controller_name = FALSE, type_opts = list()) {
   con <- file(file, encoding = encoding)
   results_raw <- readLines(con = con, warn = FALSE)
   results_raw <- gsub(",+$", "", results_raw)
-  
+
   blocks <- asplit(matrix(cumsum(rle(grepl("^#", results_raw))$lengths), ncol = 2, byrow = TRUE), 1)
   ref_lines <- c(1, sapply(blocks, `[`, 2)[-length(blocks)] + 1)
-  
+
   block_lines <- lapply(seq_along(blocks), function(i) {
     block <- lapply(blocks[[i]], function(x) {
       if (x == blocks[[i]][1]) {
@@ -30,7 +32,7 @@ read_pcibex <- function(file, encoding = "UTF-8", exclude_controller_name = FALS
     names(block) <- c("colnames", "data")
     block
   })
-  
+
   parse_colnames <- function(lines) {
     colnames_lines <- results_raw[lines]
     colnames_lines <- colnames_lines[grepl("^# \\d+", colnames_lines)]
@@ -49,24 +51,24 @@ read_pcibex <- function(file, encoding = "UTF-8", exclude_controller_name = FALS
     }
   })
   all_colnames <- unique(unlist(block_colnames, use.names = FALSE))
-  
+
   block_data <- lapply(seq_along(blocks), function(i) {
     out <- as.data.frame(do.call(rbind, strsplit(results_raw[block_lines[[i]]$data], ",")))
     colnames(out) <- block_colnames[[i]]
     out
   })
-  
+
   for (i in seq_along(block_data)) {
     diff_cols <- setdiff(all_colnames, colnames(block_data[[i]]))
     if (length(diff_cols) > 0) {
       block_data[[i]][, diff_cols] <- NA
     }
   }
-  
+
   result <- do.call(rbind, block_data)
-  
+
   close(con)
-  
+
   if (all(c("readr", "tibble") %in% rownames(utils::installed.packages()))) {
     asNamespace("tibble")$as_tibble(do.call(asNamespace("readr")$type_convert, utils::modifyList(type_opts, list(df = result))))
   } else {
